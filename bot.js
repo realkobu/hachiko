5f062d6ac2d116a186bb34db6d2d004a526c6a89
Kobu <jesusvelezmar@gmail.com>
2020-08-30 18:09:09 +0200

    Added ping and hachiko commands to the bot, also the starting with dynamic commands

---
+++ bot.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+require("dotenv").config();
+const token = process.env.DSC_TOKEN;
+const prefix = process.env.BOT_PREFIX;
+
+const discord = require("discord.js");
+const client = new discord.Client();
+
+// Command collection
+client.commands = new discord.Collection();
+
+// Searching files on commands folder
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+// Taking commands from the file
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+// On ready
+client.on('ready', () => {
+    console.log("Bot ready!");
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (erro) {
+        console.error(error);
+        message.reply("There was an error executing the command");
+    }
+
+});
+
+
+
+client.login(token);
---
+++ commands/hachiko.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'hachiko',
+    description: 'Who is hachiko',
+    execute(message, args) {
+        message.channel.send("I'm hachiko, **the bot and good dog** of this server! Nice to meet you!");
+    }
+}
