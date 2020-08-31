4bf318a3886bbd9d4c68057774bc63ddfdb42dc2
Kobu <jesusvelezmar@gmail.com>
2020-08-31 10:50:22 +0200

    Hug command

---
+++ commands/hug.js
@@ -0,0 +1,10 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "hug",
+    description: "Hug someone!",
+    execute(message, args) {
+        const embed = new MessageEmbed()
+        .setTitle("AA")
+    }
+}
